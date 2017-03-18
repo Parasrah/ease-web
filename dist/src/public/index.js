4c11206b1124f73cbfff9b7e53f03c175ac98dad
Brad Pfannmuller <bradpf@outlook.com>
2017-03-18 13:07:09 -0400

    Removing dist

--- dist/src/public/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("react-mdl/extra/material.css");
-require("react-mdl/extra/material.js");
-//# sourceMappingURL=index.js.map
