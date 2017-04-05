b997be3a05489927502356569e00cd2497573bcb
Brad Pfannmuller <Brad Pfannmuller>
2017-04-04 20:54:50 -0400

    Styling start page

--- src/view/pages/StartPage.tsx
+++ src/view/pages/StartPage.tsx
@@ -1,4 +1,6 @@
+import * as Guid from "guid";
 import * as React from "react";
+import { IconButton } from "react-mdl";
 import { connect } from "react-redux";
 
 import "../../style/start.less";
@@ -26,17 +28,44 @@ interface IStartPageState {
 type StartPageProps = IStartPageInputProps & IStartPageStoreProps & IStartPageDispatchProps;
 
 export class StartPage extends React.Component<StartPageProps, IStartPageState> {
+    private idInput: HTMLInputElement;
 
     constructor(props: StartPageProps) {
         super(props);
+
+        // Bindings
+        this.idInputKeyDown = this.idInputKeyDown.bind(this);
+        this.onIdButtonClick = this.onIdButtonClick.bind(this);
+        this.setIdInput = this.setIdInput.bind(this);
+    }
+
+    /*********************** Methods ****************************/
+
+    private submit() {
+        const guid = this.idInput.value;
+        if (guid === "") {
+            // TODO warning message
+        }
+        else if (!Guid.isGuid(guid)) {
+            // TODO warning message
+        }
+        this.props.changePageDispatch(Page.VIDEO_CLIENT);
     }
 
     /*********************** Callbacks ***************************/
 
-    private idInputKeyDown: React.EventHandler<React.KeyboardEvent<HTMLInputElement>> = (event) => {
+    private idInputKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
         if (event.key == "enter") {
-            this.props.changePageDispatch(Page.START);
+            this.submit();
         }
+    }
+
+    private onIdButtonClick() {
+        this.submit();
+    }
+
+    private setIdInput(input: HTMLInputElement) {
+        this.idInput = input;
     }
 
     /********************* React Lifecycle ***********************/
@@ -44,11 +73,23 @@ export class StartPage extends React.Component<StartPageProps, IStartPageState> 
     public render(): JSX.Element {
         return (
             <div className="start-page">
-                <input
-                    type="text"
-                    className="id-input"
-                    onKeyDown={this.idInputKeyDown}
-                />
+                <span className="logo">
+                    ease
+                </span>
+                <div className="input-wrapper">
+                    <input
+                        type="number"
+                        className="id-input"
+                        onKeyDown={this.idInputKeyDown}
+                        ref={this.setIdInput}
+                    />
+                    <IconButton
+                        className="join"
+                        name="send"
+                        onClick={this.onIdButtonClick}
+                        ripple={true}
+                    />
+                </div>
             </div>
         );
     }
