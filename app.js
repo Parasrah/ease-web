517880655afea609d51dbd3965f74ff48efc2095
Brad <bradpf@outlook.com>
2017-04-14 13:53:16 -0400

    Cursor & Favicon Fix (#74)
    * fixing cursor #61
    
    * fixing favicon route #73

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ app.get("/manifest.json", (req, res) => {
     res.sendfile(__dirname + "/manifest.json");
 })
 
-app.get("/favicon.ico", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "favicons", "favicon.ico"));
-})
-
 // Listen
 app.listen(port, "0.0.0.0", () => {
     console.log("The action happens at https://localhost:" + port);
